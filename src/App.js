9612a0428b6fbb698c234211db88e116557ee4a1
shiragariku <r.shiraga@senkyo.co>
2024-04-25 02:11:14 +0900

    useSelector使用方法学習

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
+import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <div className="App"></div>;
+  const postList = useSelector((state) => state.posts.value);
+  console.log(postList);
+  return (
+    <div className="App">
+      <div>
+        <h1>React-Redux掲示板</h1>
+      </div>
+      <div className="addPost">
+        <input type="text" placeholder="お名前" />
+        <input type="text" placeholder="投稿内容" />
+        <button>投稿</button>
+        <hr />
+      </div>
+    </div>
+  );
 }
 
 export default App;

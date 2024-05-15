0288efa91d6a6ab555c051e6cad2a198eebd6de2
shiragariku <r.shiraga@senkyo.co>
2024-05-16 02:38:19 +0900

    投稿機能実装

--- src/features/Post.js
+++ src/features/Post.js
@@ -2,13 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 import { PostsData } from "../DummyData";
 
 export const postSlice = createSlice({
-    name: "posts",
-    initialState: { value: PostsData },
-    reducers: {
-        addPost: (state, action) => {
-            state.value.push(action.payload);
-        }
-    }
+  name: "posts",
+  initialState: { value: PostsData },
+  reducers: {
+    addPost: (state, action) => {
+      state.value.push(action.payload);
+    },
+  },
 });
 
+export const { addPost } = postSlice.actions;
 export default postSlice.reducer;
